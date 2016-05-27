143b080e9863940bf7ae1b6e943f2e16fe0add07
Zibtsev Vladimir <zibtsev.vladimir@gmail.com>
2016-05-27 14:22:49 +0300

    update

--- Instagram.py
+++ Instagram.py
@@ -8,17 +8,20 @@ import re
 import json
 from functools import reduce
 import math
+import logging
+
+from pprint import pprint
 
 # FIX: класс ничего не должен сам выводить в stdout
 # он должен генерировать исключения и сообщения об ошибках.
 # Которые получаются методом err_msg и пользовательский скрипт
 # их сам уже обрабатывает!
 
+# FIX: надо сделать человеческую обработку всех возможных ошибок и исключений
+
 
 
 class Instagram(object):
-    DEBUG = True
-
     def __init__(self, login, password):
         self.__url           = 'https://www.instagram.com/'
         self.__url_tag       = self.__url + 'explore/tags/'
@@ -28,6 +31,8 @@ class Instagram(object):
         self.__url_post      = self.__url + 'query/'
         self.__url_home      = self.__url + login
         self.__url_followers = self.__url + login + '/followers/'
+
+        logging.basicConfig(level=logging.ERROR)
 
         self.__s = requests.Session()
         self.__login(login, password)
@@ -44,17 +49,23 @@ class Instagram(object):
     #       False в случае ошибки
     def get_followers(self):
         if self.__login_user:
-            # получаюпервых 1000 подпичиков. за кол-во отвечает параметр followed_by.first
+            # получаю первых 1000 подпичиков. за кол-во отвечает параметр followed_by.first
             data = self.__get_json_post(
                 self.__url_post,
                 self.__url_home,
-                'q=ig_user(' + self.__user_id + ')+%7B%0A++followed_by.first(1000)+%7B%0A++++count%2C%0A++++page_info+%7B%0A++++++end_cursor%2C%0A++++++has_next_page%0A++++%7D%2C%0A++++nodes+%7B%0A++++++id%2C%0A++++++is_verified%2C%0A++++++followed_by_viewer%2C%0A++++++requested_by_viewer%2C%0A++++++full_name%2C%0A++++++profile_pic_url%2C%0A++++++username%0A++++%7D%0A++%7D%0A%7D%0A&ref=relationships%3A%3Afollow_list',
-                'Не удалось получить данные. Сообщеение сервера: %s! Ошибка №1.'
+                ('q=ig_user({user_id})+%7B%0A++followed_by.first(1000)+%7B%0A++++count%2C%0A++++page_info+%7B%0A++++++'
+                 'end_cursor%2C%0A++++++has_next_page%0A++++%7D%2C%0A++++nodes+%7B%0A++++++id%2C%0A++++++'
+                 'is_verified%2C%0A++++++followed_by_viewer%2C%0A++++++requested_by_viewer%2C%0A++++++'
+                 'full_name%2C%0A++++++profile_pic_url%2C%0A++++++username%0A++++%7D%0A++%7D%0A%7D%0A&ref='
+                 'relationships%3A%3Afollow_list').format(user_id = self.__user_id)
             )
+            if data == False:
+                logging.error('Не удалось получить список подписчиков.')
+                return False
 
             return data['followed_by']['nodes']
         else:
-            self.__write_log('Пользователь не авторизован! Ошибка №2.')
+            logging.error('Пользователь не авторизован.')
             return False
 
     # Получить список тегов других пользователей, которые связанные с моими тегами
@@ -65,32 +76,44 @@ class Instagram(object):
         if self.__login_user:
             dict_tags = {}
             tags = self.__raw_caption2tags(self.__get_my_media())
+            if tags == False:
+                logging.error('Ошибка при получении списка тегов других пользователей.')
+                return False
 
             for tag in tags:
-                dict_tags[tag] = self.__raw_caption2tags(self.__get_media_tag(tag, depth))
+                d = self.__raw_caption2tags(self.__get_media_tag(tag, depth))
+                if d != False:
+                    dict_tags[tag] = d
 
             return dict_tags
         else:
-            self.__write_log('Пользователь не авторизован! Ошибка №3.')
+            logging.error('Пользователь не авторизован.')
             return False
 
     # Автоматическое расставление лайков.
     # Берутся все мои публикации, по каждому тегу получаем список чужих публикаций.
     # Из этого списка выбираются те укоторых лайков меньше Х (например 300) и которые ещё не лайкнуты.
     # И все эти публикации лайкаются с задержкой по времени 60+rand сек
-    # Все лайкнутые материалы сохраняются.
-    def likes(self):
+    def likes(self, depth = 100):
         if self.__login_user:
             tags = self.__raw_caption2tags(self.__get_my_media())
+            if tags == False:
+                logging.error('Не удалось выполнить функцию likes()')
+                return False
+            random.shuffle(tags) # случайно мешаем теги
+
             for tag in tags:
-                data = self.__get_media_tag(tag, 100)
-                for d in data:
-                    if self.DEBUG:
-                        print('TAG: %s URL: ' % tag, end = '')
-                    self.__like(d, 100)
+                print('Тег: %s' % tag)
+                data = self.__get_media_tag(tag, depth)
+                if data != False:
+                    for d in data:
+                        logging.info('TAG: %s' % tag)
+                        self.__sleep(30)
+                        self.__like(d, depth)
+
             return True
         else:
-            self.__write_log('Пользователь не авторизован! Ошибка №4.')
+            logging.error('Пользователь не авторизован.')
             return False
 
     ###
@@ -136,7 +159,7 @@ class Instagram(object):
             return True
         except Exception:
             self.__login_user = False
-            self.__write_log('Проблемы с сетью! Ошибка №5.')
+            logging.critical('Проблемы при обращению к серверу при авторизации пользователя %s.' % user_login)
             return False
 
     # Проверка авторизации пользователя.
@@ -144,29 +167,30 @@ class Instagram(object):
     #      строка с логином пользователя
     # Выход True в случае успешной проверки
     def __login_check(self, login_r, user_login):
-        if login_r.status_code == 200:
+        status_code = login_r.status_code
+        if status_code == 200:
             try:
                 r = self.__s.get(self.__url)
                 f = r.text.find(user_login)
 
                 if f != -1:
                     self.__login_user = True
-                    self.__user_id = r.cookies['ds_user_id']  # типа индетификатор пользователя получаю
-                    self.__write_log('Авторизация пользователя %s прошла успешно.' % user_login)
+                    self.__user_id    = r.cookies['ds_user_id']  # типа индетификатор пользователя получаю
+                    logging.info('Авторизация пользователя %s прошло успешно.' % user_login)
                     # FIX: нужно добавить возможность сохранять сессию в файл
                     #      чтобы каждый раз не автоизироваться
                     return True
                 else:
-                    self.__write_log('Логин/пароль не корректен! Ошибка №6.')
                     self.__login_user = False
+                    logging.error('Авторизация пользователя %s не удалась.' % user_login)
                     return False
             except Exception:
                 self.__login_user = False
-                self.__write_log('Проблемы с сетью! Ошибка №7.')
+                logging.critical('Проблемы при обращению к серверу при авторизации пользователя %s.' % user_login)
                 return False
         else:
-            self.__write_log('Сервер выдал ошибку приавторизации! Ошибка №8.')
             self.__login_user = False
+            logging.critical('Сервер выдал статус %s.' % status_code)
             return False
 
     # Выход пользователя с сервера
@@ -182,14 +206,13 @@ class Instagram(object):
                 )
                 self.__login_user = False
 
-                if self.DEBUG:
-                    self.__write_log('Пользователь вышел успешно.')
+                logging.info('Пользователь вышел успешно.')
                 return True
             else:
-                self.__write_log('Пользователь не авторизован! Ошибка №9.')
+                logging.error('Пользователь не авторизован.')
                 return False
         except Exception:
-            self.__write_log('Сервер выдал ошибку при выходе пользователя! Ошибка №10.')
+            logging.critical('Проблемы при обращению к серверу при выходе пользователя.')
             return False
 
     # Получить список публикаций по указанному тегу
@@ -203,32 +226,38 @@ class Instagram(object):
         data_all = []
         # Делаем первый запрос методом GET и получаем первую пачку (12) публикаций,
         # а так же и параметры для последующих POST запросов
-        data_json, cursor = self.__get_media_tag_first(tag)
+        tag_data = self.__get_media_tag_first(tag)
+        if tag_data == False:
+            return False
 
         # Если запрошенных публикаций <= 12, то возвращаем данные
         # в противном случае делаем необходимое кол-во POST запросов чтобы
         # набрать нужное кол-во данных
         if number <= 12:
-            return data_json
+            return tag_data[0]
 
         # Обновляем заголовок и отправляем нужное кол-во POST запросов
-        data_all.append(data_json)
+        data_all.append(tag_data[0])
 
-        media_after = cursor[1]  # end_cursor
+        media_after = tag_data[1][1]  # end_cursor
         i = 1
         while i < math.ceil(number/12.0):
             i += 1
 
-            #self.__sleep(30) # Засыпаем на 30сек между запросами к серверу
+            self.__sleep(30)
             data = self.__get_json_post(
                 self.__url_post,
                 self.__url_tag + urllib.parse.quote(tag),
-                'q=ig_hashtag(' + tag + ')+%7B+media.after(' + media_after + '%2C+12)+%7B%0A++count%2C%0A++nodes+%7B%0A++++caption%2C%0A++++code%2C%0A++++comments+%7B%0A++++++count%0A++++%7D%2C%0A++++date%2C%0A++++dimensions+%7B%0A++++++height%2C%0A++++++width%0A++++%7D%2C%0A++++display_src%2C%0A++++id%2C%0A++++is_video%2C%0A++++likes+%7B%0A++++++count%0A++++%7D%2C%0A++++owner+%7B%0A++++++id%0A++++%7D%2C%0A++++thumbnail_src%0A++%7D%2C%0A++page_info%0A%7D%0A+%7D&ref=tags%3A%3Ashow',
-                'Не удалось получить данные. Сообщение сервера: %s! Ошибка №11.'
-            )
-
-            media_after = data['media']['page_info']['end_cursor']
-            data_all.append(data['media']['nodes'])
+                ('q=ig_hashtag({tag})+%7B+media.after({media_after}%2C+12)+%7B%0A++count%2C%0A++nodes+%7B%0A++++'
+                 'caption%2C%0A++++code%2C%0A++++comments+%7B%0A++++++count%0A++++%7D%2C%0A++++date%2C%0A++++'
+                 'dimensions+%7B%0A++++++height%2C%0A++++++width%0A++++%7D%2C%0A++++display_src%2C%0A++++id%2C%0A++++'
+                 'is_video%2C%0A++++likes+%7B%0A++++++count%0A++++%7D%2C%0A++++owner+%7B%0A++++++id%0A++++%7D%2C%0A++++'
+                 'thumbnail_src%0A++%7D%2C%0A++page_info%0A%7D%0A+%7D&'
+                 'ref=tags%3A%3Ashow').format(tag = tag, media_after = media_after)
+                )
+            if data != False:
+                media_after = data['media']['page_info']['end_cursor']
+                data_all.append(data['media']['nodes'])
 
         return self.__list2list(data_all)
 
@@ -238,9 +267,10 @@ class Instagram(object):
     #                 [1] - [start_cursor, end_cursor]
     def __get_media_tag_first(self, tag):
         data = self.__get_json_get(
-            self.__url_tag + urllib.parse.quote(tag),
-            'Не удалось получить список публикаций по тегу %s! Ошибка №12.' % tag
+            self.__url_tag + urllib.parse.quote(tag)
         )
+        if data == False:
+            return False
         cursor = [
             data['entry_data']['TagPage'][0]['tag']['media']['page_info']['start_cursor'],
             data['entry_data']['TagPage'][0]['tag']['media']['page_info']['end_cursor']
@@ -254,7 +284,9 @@ class Instagram(object):
     # Выход json данные в случае успеха
     #       False в случае ошибки
     def __get_my_media(self):
-        json_data  = self.__get_json_get(self.__url_home, 'Не удалось получить список своих публикаций. Ошибка №3')
+        json_data  = self.__get_json_get(self.__url_home)
+        if json_data == False:
+            return False
         count      = json_data['entry_data']['ProfilePage'][0]['user']['media']['count'] # число, сколько публикаций всего
         data       = json_data['entry_data']['ProfilePage'][0]['user']['media']['nodes']
         end_cursor = json_data['entry_data']['ProfilePage'][0]['user']['media']['page_info']['end_cursor']
@@ -265,15 +297,20 @@ class Instagram(object):
             while i < math.ceil(count/12.0):
                 i += 1
 
+                self.__sleep(30)
                 d = self.__get_json_post(
                     self.__url_post,
                     self.__url_home,
-                    'q=ig_user(' + self.__user_id + ')+%7B+media.after(' + end_cursor + '%2C+12)+%7B%0A++count%2C%0A++nodes+%7B%0A++++caption%2C%0A++++code%2C%0A++++comments+%7B%0A++++++count%0A++++%7D%2C%0A++++date%2C%0A++++dimensions+%7B%0A++++++height%2C%0A++++++width%0A++++%7D%2C%0A++++display_src%2C%0A++++id%2C%0A++++is_video%2C%0A++++likes+%7B%0A++++++count%0A++++%7D%2C%0A++++owner+%7B%0A++++++id%0A++++%7D%2C%0A++++thumbnail_src%0A++%7D%2C%0A++page_info%0A%7D%0A+%7D&ref=users%3A%3Ashow',
-                    'Не удалось получить данные. Сообщение сервера: %s! Ошибка №13.'
+                    ('q=ig_user({user_id})+%7B+media.after({end_cursor}%2C+12)+%7B%0A++count%2C%0A++nodes+%7B%0A++++'
+                     'caption%2C%0A++++code%2C%0A++++comments+%7B%0A++++++count%0A++++%7D%2C%0A++++date%2C%0A++++'
+                     'dimensions+%7B%0A++++++height%2C%0A++++++width%0A++++%7D%2C%0A++++display_src%2C%0A++++'
+                     'id%2C%0A++++is_video%2C%0A++++likes+%7B%0A++++++count%0A++++%7D%2C%0A++++owner+%7B%0A++++++'
+                     'id%0A++++%7D%2C%0A++++thumbnail_src%0A++%7D%2C%0A++page_info%0A%7D%0A+%7D&'
+                     'ref=users%3A%3Ashow').format(user_id = self.__user_id, end_cursor= end_cursor)
                 )
-
-                end_cursor = d['media']['page_info']['end_cursor']
-                data.extend( d['media']['nodes'] )
+                if d != False:
+                    end_cursor = d['media']['page_info']['end_cursor']
+                    data.extend( d['media']['nodes'] )
 
         return data
 
@@ -284,10 +321,12 @@ class Instagram(object):
     def __like(self, data, like_count):
         if data['likes']['count'] < like_count:
             code = data['code']
-            d = self.__get_json_get(
-                'https://www.instagram.com/p/%s' % code,
-                'Не удалось получить данные с сервера. Ошибка №14.'
-            )
+            url = 'https://www.instagram.com/p/%s' % code
+            d = self.__get_json_get(url)
+            if d == False:
+                logging.error('Не удалось лайкнуть публикацию по адресу %s' % url)
+                return False
+
             try:
                 id    = d['entry_data']['PostPage'][0]['media']['id']
                 liked = d['entry_data']['PostPage'][0]['media']['likes']['viewer_has_liked']
@@ -295,10 +334,12 @@ class Instagram(object):
                 if liked == False:
                     url = self.__url + 'web/likes/%s/like/' % id
                     ref = self.__url + 'p/%s/' % code
-                    self.__get_json_post(url, ref, {}, 'Не удалось поставить like на публикации %s! Ошибка №15.' % code)
-                    if self.DEBUG:
-                        print(ref)
+                    self.__get_json_post(url, ref, {})
+                    self.__sleep(30)
+
+                    logging.info('URL: %s' % ref)
             except Exception:
+                logging.error('Непредвиденная ошибка при лайке публикации по адресу %s' % url)
                 return False
 
         return True
@@ -308,10 +349,9 @@ class Instagram(object):
     #      строка с сообщением об ошибке
     # Выход json данные в слуае успеха
     #       False в случае ошибки
-    def __get_json_get(self, url, error_msg):
+    def __get_json_get(self, url):
         try:
             r = self.__s.get(url)
-            self.__sleep(10)
             text = r.text
 
             text_start     = ('<script type="text/javascript">window._sharedData = ')
@@ -321,9 +361,10 @@ class Instagram(object):
             all_data_start = text.find(text_start)
             all_data_end   = text.find(text_end, all_data_start + 1)
             json_str       = text[(all_data_start + text_start_len + 1): all_data_end]
+
             return json.loads(json_str)
         except Exception:
-            self.__write_log('Невозможно получить данные из сети! %s Ошибка №16.' % error_msg)
+            logging.error('Ошибка при отправке GET запроса по адресу %s.' % url)
             return False
 
     # Отправка POST запроса
@@ -333,21 +374,22 @@ class Instagram(object):
     #      строка с сообщением об ошибке
     # Возвращаются json данные в случае успеха
     #              False в случае ошибки
-    def __get_json_post(self, url, referer, data, error_msg):
+    def __get_json_post(self, url, referer, data):
         try:
             self.__s.headers.update({
                 'Referer': referer
             })
             r = self.__s.post(url, data = data)
-            self.__sleep(30)
             d = json.loads(r.text)
 
             if d['status'] == 'fail':
-                self.__write_log(error_msg % d['message'])
+                logging.error('Ошибка при отправле POST запроса. Сервер вернул сообщение: %s.' % d['message'])
                 return False
+
             return d
         except Exception:
-            self.__write_log('Невозможно получить данные из сети! %s Ошибка №17.' % error_msg)
+            logging.error('Ошибка при отправке POST запроса.')
+            logging.error('URL: ' + url + ' DATA: ' + data)
             return False
 
     ###
@@ -363,18 +405,14 @@ class Instagram(object):
     def __list2list(self, l):
         return list(reduce(lambda a, b: a + b, l, []))
 
-    # Ведение лога
-    def __write_log(self, text):
-        # Если включена отладка, то выводим в консоль сообщения
-        if self.DEBUG:
-            print(text)
-
     # Засыпаем между запросами к серверу
     def __sleep(self, t = 5):
         time.sleep(t * random.random())
 
     # Из массива json данных получаем список тегов публикаций без дублироания
     def __raw_caption2tags(self, data):
+        if data == False:
+            return False
         # Регулярным выражением из описания получаем список тегов
         # эта функция применяется к каждой публикации изполученного массива
         tags = map(lambda item: re.findall('#(\w+)', item.get('caption', '')), data)
--- likes.py
+++ likes.py
@@ -1,6 +1,11 @@
 # -*- coding: utf-8 -*-
+from datetime import datetime
 from Instagram import Instagram
 from config import *
 
+print('LIKES Date: %s' % datetime.strftime(datetime.now(), "%Y.%m.%d %H:%M:%S"))
+
 inst = Instagram(USER_LOGIN, USER_PASSWORD)
-inst.likes(100)
+inst.likes()
+
+print('FINISH')
